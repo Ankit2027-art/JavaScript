6a7f4adaa55d5e06f47b041ae3004f81658603bc
Ankit Patel <ankit8400764325@gmail.com>
2026-03-28 20:10:35 +0000

    Variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144523
+let accountEmail ="Ankit@gmail.com"
+var accountpawword ="4544354"
+let accountState;
+
+// accountId =2// we can't change const funs value
+/*
+  Not use to prefer var because
+  of issue in block scope and funstional space.
+*/
+
+console.log(accountId);
+
+
+accountEmail ="jgh25@.com"
+accountPassword ="656532323"
+accountCity="Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
