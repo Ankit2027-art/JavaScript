96e1106028cf72dccd7f5d3eb31ad4751a2815f6
Ankit Patel <ankit8400764325@gmail.com>
2026-03-28 20:10:04 +0000

    DataType

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use Strict"; //trate all js code as new version
+
+//alert(3 + 3) //we are using nodejs ,not browser
+
+
+let name ="Ankit"
+let age =18
+let isLoggedIn = false
+
+//number =>2 to power 53(range)
+//bigint-use for log data
+//string => ""
+//null => standalone value
+// undefine => "Value not Define"
+//symbol => use for unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);  //null is a object
