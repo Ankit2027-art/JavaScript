dce3c20c985d47ddd91272b608383b26c8050338
Ankit Patel <ankit8400764325@gmail.com>
2026-03-30 15:37:01 +0000

    String

---
+++ 01_basics/07_string.js
@@ -0,0 +1,19 @@
+const name = "Ankit"
+const repoCount = 50;
+
+//console.log(name + repoCount + "value");
+
+// String intropolation
+console.log(`Hello my name is: ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String("Ankitt");
+
+
+console.log("The Game name is:",gameName[0]);
+
+console.log(gameName.__proto__);
+
+
+
+
+
