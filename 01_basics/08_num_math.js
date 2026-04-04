5e4472906e3c7a7bebbdeff9a7cb6a6fa2c644b9
Ankit Patel <ankit8400764325@gmail.com>
2026-04-04 08:31:53 +0000

    num

---
+++ 01_basics/08_num_math.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score)
+console.log(typeof score)
+
+
+const balence = new  Number(100)
+console.log(balence)
+console.log(typeof balence)
+
+console.log(balence.toString().length);
+console.log(balence.toFixed(2))
+
+const otherNumber = 123.8966
+//gives round off
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+//add coma
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
